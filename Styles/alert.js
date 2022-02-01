53d8a63ce2fab514c69cd66c7932e210a8523b29
anandkumarcoder <akanand104@gmail.com>
2022-02-01 18:52:30 +0530

    alert close btn done

---
+++ Styles/alert.js
@@ -0,0 +1,6 @@
+const alertBox = document.querySelector("#alert-box-close");
+const closeBtn = document.querySelector("#alert-example-close-btn");
+
+closeBtn.addEventListener("click", ()=> {
+alertBox.style.display = "none"
+})
